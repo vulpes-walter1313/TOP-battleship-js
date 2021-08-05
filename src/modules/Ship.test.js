b61db03d0906ba05ef25dd0f19fb18f3bbbbb0b7
Walter Fritsche <wfritsche95@protonmail.com>
2021-08-05 15:52:25 -0500

    updated hit method tests to check for current implementation

--- src/modules/Ship.test.js
+++ src/modules/Ship.test.js
@@ -14,27 +14,28 @@ describe('Ship instantiation test', () => {
 
 });
 describe('hit method tests', () => {
-  
+
   test('expect hit to increase ship.hits.length', () => {
     const destroyer = new Ship(5);
-    destroyer.hit(15);
+    destroyer.hit([0,0]);
     expect(destroyer.hits.length).toBe(1);
   });
   
   test('expect hit position to be in ship.hits array', () => {
     const destroyer = new Ship(5);
-    destroyer.hit(14);
-    expect(destroyer.hits[0]).toBe(14);
+    destroyer.hit([3,2]);
+    expect(destroyer.hits).toContainEqual([3,2]);
   });
 
   test('ship.hits should contain all the hits given', () => {
     const destroyer = new Ship(5);
-    destroyer.hit(14);
-    destroyer.hit(7);
-    destroyer.hit(25);
-    expect(destroyer.hits).toContain(14);
-    expect(destroyer.hits).toContain(25);
-    expect(destroyer.hits).toContain(7);
+    destroyer.hit([0,0]);
+    destroyer.hit([1,0]);
+    destroyer.hit([2,0]);
+    expect(destroyer.hits).toContainEqual([0,0]);
+    expect(destroyer.hits).toContainEqual([1,0]);
+    expect(destroyer.hits).toContainEqual([2,0]);
+    expect(destroyer.hits).not.toContainEqual([3,0]);
   });
 
 });
