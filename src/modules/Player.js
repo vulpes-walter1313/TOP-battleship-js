fc02d73d834bffc0e473a368ba2b31c96f8d68ef
Walter Fritsche <wfritsche95@protonmail.com>
2021-08-16 14:31:04 -0500

    added player and player tests

---
+++ src/modules/Player.js
@@ -0,0 +1,28 @@
+import Gameboard from "./Gameboard";
+
+class Player {
+  constructor(name) {
+    this.name = name || 'computer';
+    this.gameboard = new Gameboard();
+    this.enemyBoard = new Gameboard();
+  }
+
+  computerMove() {
+    let firstIndex = this.randomNumGen();
+    let secondIndex = this.randomNumGen();
+
+    while (this.enemyBoard.board[firstIndex][secondIndex].hasBeenShot === true) {
+      firstIndex = this.randomNumGen();
+      secondIndex = this.randomNumGen();
+    }
+    
+    return [firstIndex, secondIndex];
+  }
+
+  randomNumGen() {
+    const randomNumber = Math.floor(Math.random() * (9 - 0) + 0);
+    return randomNumber;
+  }
+}
+
+export default Player;
---
+++ src/modules/Player.test.js
@@ -0,0 +1,22 @@
+import Player from './Player';
+import Ship from './Ship';
+
+describe('Player init Structure', () => {
+  test('Player has gameboard', () => {
+    const player = new Player('Karl');
+    expect(player.gameboard.board[0].length).toBe(10);
+    expect(player.gameboard.board[0][0]).toEqual({hasShip: false, hasBeenShot: false});
+  });
+});
+
+describe('Computer Attack test', () => {
+  
+  test('Computer can make a valid move', () => {
+    const computer = new Player();
+    const coords = computer.computerMove();
+    expect(coords[0]).toBeGreaterThanOrEqual(0);
+    expect(coords[1]).toBeLessThanOrEqual(9);
+    expect(coords.length).toBe(2);
+  });
+
+});
